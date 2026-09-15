84b829df6f464098197c171c0a1587fd57454f38
znseaman <znseaman@gmail.com>
2026-09-15 09:57:51 -0700

    fix: let the framed page run JavaScript

--- src/views/checkout.ts
+++ src/views/checkout.ts
@@ -62,7 +62,7 @@ export function renderCheckoutPage(
         <article class="card">
           <h2>Delivery Estimate</h2>
           <p>Check when Acorn Express can deliver your new friend.</p>
-          <iframe class="shipping-widget" src="/shipping-widget.html" title="Acorn Express shipping estimate"></iframe>
+          <iframe sandbox="allow-scripts" class="shipping-widget" src="/shipping-widget.html" title="Acorn Express shipping estimate"></iframe>
         </article>
       </section>`,
   );
